1b64daa357db9868bcf00c9d9a7ebde598fe1ecb
Khushi2804 <162557539+Khushi2804@users.noreply.github.com>
2024-11-21 08:22:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+fun
